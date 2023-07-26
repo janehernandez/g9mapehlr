3be57240d3c0a416504e4d5c25ccf35ff7cb36c0
Jane <kitkatdonat@gmail.com>
2023-07-26 22:16:35 +0800

    Finalize Links

--- js/fetch.js
+++ js/fetch.js
@@ -3,14 +3,13 @@ fetch("../json/index.json")
     return res.json();
   })
   .then((data) => {
-    const { announcement, teachers } = data;
-    $(".announcement").text(announcement);
-
+    const { announcements, teachers, menus } = data;
     displayTeacherSlide(teachers);
+    displayAnnouncementSlides(announcements);
+    displayMenus(menus);
   });
 
-function displayTeacherSlide(teachers) {
-  const { g9: data } = teachers;
+function displayTeacherSlide({ g9: data }) {
   const owlCarousel = $(".teacher-carousel");
 
   // Loop through each item in the JSON data
@@ -32,17 +31,7 @@ function displayTeacherSlide(teachers) {
     // Create the designation element (p)
     const designationParagraph = $("<p>").text(item.designation);
 
-    // Create the teacher-text div element
-    const teacherTextDiv = $("<div>").addClass(
-      "teacher-text bg-light text-center p-4"
-    );
-
-    // Create the motto in life paragraph element
-    const mottoParagraph = $("<p>").addClass("mb-0").text(item.motto);
-
-    // Append the elements to their respective parent elements
-    teacherTextDiv.append(mottoParagraph);
-    slideDiv.append(img, nameHeading, designationParagraph, teacherTextDiv);
+    slideDiv.append(img, nameHeading, designationParagraph);
 
     // Append the slide to the Owl Carousel
     owlCarousel.trigger("add.owl.carousel", [slideDiv]);
@@ -50,4 +39,79 @@ function displayTeacherSlide(teachers) {
 
   // Refresh the Owl Carousel to display the new slides
   owlCarousel.trigger("refresh.owl.carousel");
+}
+
+function displayAnnouncementSlides(announcements) {
+  const owlCarousel = $(".announcement-carousel");
+
+  // Loop through each item in the JSON data
+  announcements.forEach((item) => {
+    console.log(item);
+    // Create the slide div element for the announcement item
+    const slideDiv = $("<div>").addClass("announcement-item text-center");
+
+    // Create the teacher-text div element
+    const announcementDiv = $("<div>").addClass(
+      "announcement-text bg-light text-center p-4"
+    );
+
+    const contentParagraph = $("<p>").addClass("mb-4").text(item.content);
+
+    let link = null;
+    if (item.link) {
+      link = $("<a>")
+        .addClass("mt-3 italic")
+        .attr("href", item.link)
+        .attr("target", "_blank")
+        .text("Click Here to view details");
+    }
+
+    announcementDiv.append(contentParagraph, link);
+    slideDiv.append(announcementDiv);
+
+    // Append the slide to the Owl Carousel
+    owlCarousel.trigger("add.owl.carousel", [slideDiv]);
+  });
+
+  // Refresh the Owl Carousel to display the new slides
+  owlCarousel.trigger("refresh.owl.carousel");
+}
+
+function displayMenus(menus) {
+  const navBar = $(".navbar-nav");
+
+  menus.forEach((menu, key) => {
+    let menuLink = null;
+    let navBarDropdown = null;
+    if (!menus[key].sub_menu) {
+      menuLink = $("<a>")
+        .addClass("nav-item nav-link")
+        .attr("href", menus[key].link)
+        .attr("target", "_blank")
+        .text(menus[key].label);
+    } else {
+      navBarDropdown = $("<div>").addClass("nav-item dropdown");
+
+      const childMenuLink = $("<a>")
+        .addClass("nav-link dropdown-toggle")
+        .attr("href", menus[key].link)
+        .attr("data-bs-toggle", "dropdown")
+        .text(menus[key].label);
+
+      const subMenuDiv = $("<div>").addClass("dropdown-menu fade-down m-0");
+
+      menus[key].sub_menu.forEach((subMenuItem, subMenuKey) => {
+        const subMenuLink = $("<a>")
+          .addClass("dropdown-item")
+          .attr("href", subMenuItem.link)
+          .attr("target", "_blank")
+          .text(subMenuItem.label);
+        subMenuDiv.append(subMenuLink);
+      });
+
+      navBarDropdown.append(childMenuLink, subMenuDiv);
+    }
+
+    navBar.append(menuLink, navBarDropdown);
+  });
 }
